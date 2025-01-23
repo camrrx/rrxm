f3c189ebf59c1a99a4511b26cc11ac81d44d95eb
camrrx <camille.rx@free.fr>
2025-01-23 16:53:20 +0100

    new icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		VitePWA({
+			manifest: {
+				name: "RRX Movies",
+				short_name: "RRXM",
+				icons: [
+					{
+						src: "/logo192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "logo512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+				start_url: "/",
+				display: "standalone",
+				background_color: "#ffffff",
+				theme_color: "#317EFB",
+			},
+		}),
+	],
+});
